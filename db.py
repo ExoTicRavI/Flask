ab03fe2fcb7bb87a7290628a6da59a460a0bf652
Sashi <sashilal120@gmail.com>
2026-06-15 10:02:52 +0545

    flask database crud

--- db.py
+++ db.py
@@ -8,22 +8,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class student(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     email = db.Column(db.String(100), unique = True, nullable = False)
 
 @app.route('/add')
 def addstudent():
-    Student = student(name = 'Sashi kumar lal', email = 'sashilal120@gmail.com')
+    students = Student(name = 'Sashi kumar lal', email = 'sashilal120@gmail.com')
     db.session.add(Student)
     db.session.commit()
     return "Student added to our database!"
 
 @app.route('/students')
 def list_students():
-    Students = student.query.all()
-    return '<br>'.join([f"{s.id}. {s.name} - {s.email}" for s in Students])
+    students = Student.query.all()
+    return '<br>'.join([f"{s.id}. {s.name} - {s.email}" for s in students])
 
 
 @app.route('/')
